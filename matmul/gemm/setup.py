d1d610002ec3b5791a99da9ed120bd0ea9725f85
duanyunzhi <duanyunzhi@infini-ai.com>
2025-07-13 07:51:03 +0000

    perf gemm

--- matmul/gemm/setup.py
+++ matmul/gemm/setup.py
@@ -7,7 +7,7 @@ setup(
     ext_modules=[
         CppExtension(
             name='flash_fusion',  # This defines TORCH_EXTENSION_NAME
-            sources=['gemm_v3.cu']
+            sources=['gemm_v4.cu']
             # extra_compile_args= {
             #     'nvcc': [
             #         "arch=compute_89"
